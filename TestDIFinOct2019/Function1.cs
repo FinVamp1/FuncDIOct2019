1591e1da908538530b1a67561b3e84dac4b3fc21
Finbar Ryan <finbarr@microsoft.com>
2019-10-31 12:18:20 +0000

    Added missing samples from the cusotmer scenario.

--- TestDIFinOct2019/Function1.cs
+++ TestDIFinOct2019/Function1.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Logging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TestDIFinOct2019
@@ -14,13 +16,17 @@ namespace TestDIFinOct2019
     public class Function1
     {
         private readonly ParserApiClient _apiClient;
-        public Function1(ParserApiClient apiClient)
+        private readonly ILogger log;
+        private readonly IConfiguration _configuration;
+        public Function1(IConfiguration configuration, ILoggerFactory loggerFactory, ParserApiClient apiClient)
         {
+            this._configuration = configuration;
+            log = loggerFactory.CreateLogger(LogCategories.CreateFunctionCategory("common"));
             _apiClient = apiClient;
         }
 
         [FunctionName("Function1")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get","post", Route = null)] HttpRequest req)
         {
             log.LogInformation($"C# HTTP trigger function executed at: {DateTime.Now}");
             var data = "test=something";
--- TestDIFinOct2019/Startup.cs
+++ TestDIFinOct2019/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 [assembly: FunctionsStartup(typeof(TestDIFinOct2019.Startup))]
 namespace TestDIFinOct2019
@@ -14,10 +15,16 @@ namespace TestDIFinOct2019
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables()
+               .Build();
+            builder.Services.AddSingleton(config);
             builder.Services.AddHttpClient("PostMessageAPI", x =>
             {
  
-                x.BaseAddress = new Uri("http://requestbin.net/r/1lowbys1");
+                x.BaseAddress = new Uri("https://testpostaccept20191029124137.azurewebsites.net/Weatherforecast");
                 x.Timeout = TimeSpan.FromMilliseconds(65536);
             });
             builder.Services.AddSingleton<ParserApiClient>();
